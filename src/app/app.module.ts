115c67e2e8f715935d344ab6274985452db092c3
jodu07 <jhonnat.dussan@gmail.com>
2022-04-06 02:40:40 -0500

    preparation project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IngresarGastoComponent } from './components/ingresar-gasto/ingresar-gasto.component';
+import { ListaGastosComponent } from './components/lista-gastos/lista-gastos.component';
+import { GastosComponent } from './pages/gastos/gastos.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IngresarGastoComponent,
+    ListaGastosComponent,
+    GastosComponent
+   
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/lista-gastos/lista-gastos.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ListaGastosComponent } from './lista-gastos.component';
+
+describe('ListaGastosComponent', () => {
+  let component: ListaGastosComponent;
+  let fixture: ComponentFixture<ListaGastosComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ListaGastosComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ListaGastosComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
