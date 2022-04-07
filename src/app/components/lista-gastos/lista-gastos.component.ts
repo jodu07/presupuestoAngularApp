88693e609f2eb320ac48d6728db4d8be3e1957a6
jodu07 <jhonnat.dussan@gmail.com>
2022-04-07 02:23:05 -0500

    component listaGastos

--- src/app/components/lista-gastos/lista-gastos.component.ts
+++ src/app/components/lista-gastos/lista-gastos.component.ts
@@ -12,37 +12,35 @@ export class ListaGastosComponent implements OnInit {
   listaGastos:any[]=[];
   presupuesto: number;
   restante: number;
-  subscripcion: Subscription;
+  subscription: Subscription;
 
   constructor(private _presupuestoService: PresupuestoService) { 
     this.presupuesto = 0;
     this.restante = 0;
 
-    this.subscripcion = this._presupuestoService
-
-
+    this.subscription = this._presupuestoService.getGatos()
+    .subscribe( data => {
+      console.log(data);
+      this.restante = this.restante - data.cantidad;
+      this.listaGastos.push(data);
+    });
   }
-
-
-
 
   aplicarColorRestante(){
-
+    if(this.presupuesto / 4 > this.restante){
+      return 'alert alert-danger';
+    }else if(this.presupuesto / 2 > this.restante){
+      return 'alert alert-warning';
+    }else{
+      return 'alert alert-secondary';
+    }
   }
 
-
-
-
-
-
-
-
-
-
-
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 
   ngOnInit(): void {
-
     this.presupuesto = this._presupuestoService.presupuesto;
     this.restante = this._presupuestoService.restante
   }
